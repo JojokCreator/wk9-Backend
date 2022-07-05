472eb87324b90bc8bbaec894b4813c77629f4828
JojokCreator <joeyk2k@hotmail.com>
2022-07-05 18:52:32 +0100

    cors fic

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 app.use('/images', express.static('images'))
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://projectweeksbootcampersocial.herokuapp.com/"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
