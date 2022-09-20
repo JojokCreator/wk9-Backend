cdd5597a06da5de00d6332481f7ee4bea2c66c86
Joe Klakus <joeyk2k@hotmail.com>
2022-09-20 18:21:19 +0100

    noenv

--- routes/events.js
+++ routes/events.js
@@ -28,11 +28,6 @@ const eventsRouter = express.Router();
 // const upload = multer({ storage: storage });
 
 const s3 = new S3Client({
-	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-	},
-	region: process.env.AWS_REGION
 });
 
 const uploadS3 = multer({
