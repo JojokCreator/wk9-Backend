131b90d3498c0ca52a00aafbbb60d13b7b422167
Joe Klakus <joeyk2k@hotmail.com>
2022-09-20 20:53:48 +0100

    push

--- routes/events.js
+++ routes/events.js
@@ -33,7 +33,7 @@ const uploadS3 = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'cyclic-erin-python-wig-eu-west-2',
-		acl: 'public-read-write',
+		acl: 'private',
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
